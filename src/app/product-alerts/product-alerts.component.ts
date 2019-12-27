1e004b97b6000911a0df2819b5c5f09a89534005
NagendraMSITian <nag.iiitn08@gmail.com>
2019-12-27 15:14:03 +0530

    3 files modified, 3 added

---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+import {EventEmitter, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css']
+})
+export class ProductAlertsComponent implements OnInit {
+  @Input() product;
+  @Output() notifyMe = new EventEmitter();
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
